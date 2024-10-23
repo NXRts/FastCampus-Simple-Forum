4d161267fa5b0c7c3a47d7cc97f170687da434b8
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-10-23 10:46:35 +0700

    Membuat repository untuk Create User

--- internal/repository/memberships/users.go
+++ internal/repository/memberships/users.go
@@ -17,3 +17,19 @@ func (r *repository) GetUser(ctx context.Context, email, username string) (*memb
 	}
 	return &response, nil
 }
+
+//	func (r *repository) CreateUser(ctx context.Context, model memberships.UserModel) error {
+//		query := `INSERT INTO users (email, password, username, created_at, created_by, upadted_at, updated_by) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+//		_, err := r.db.ExecContext(ctx, query, model.Email, model.Password, model.CreatedAt, model.CreatedBy, model.UpdateAt, model.UpdateBy)
+//		if err != nil {
+//			return err
+//		}
+//	}
+func (r *repository) CreateUser(ctx context.Context, model memberships.UserModel) error {
+	query := `INSERT INTO users (email, password, username, created_at, created_by, updated_at, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?)`
+	_, err := r.db.ExecContext(ctx, query, model.Email, model.Password, model.Username, model.CreatedAt, model.CreatedBy, model.UpdateAt, model.UpdateBy)
+	if err != nil {
+		return err
+	}
+	return nil
+}
