e087daa5f2941feb6462afaf9bd18235fdcc64c2
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-10-23 10:29:56 +0700

    Membuat repository untuk Get User untuk validasi user sudah exist atau

---
+++ internal/repository/memberships/users.go
@@ -0,0 +1,19 @@
+package memberships
+
+import (
+	"context"
+
+	"github.com/NXRts/fsatcampus/internal/model/memberships"
+)
+
+func (r *repository) GetUser(ctx context.Context, email, username string) (*memberships.UserModel, error) {
+	query := `SELECT id, email, password, username, created_at, created_by, upadted_at, updated_by FROM users WHERE email = $1 OR username = $`
+	row := r.db.QueryRowContext(ctx, query, email, username)
+
+	var response memberships.UserModel
+	err := row.Scan(&response.ID, &response.Email, &response.Password, &response.Username, &response.CreatedAt, &response.UpdateAt, &response.CreatedBy, &response.UpdateBy)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
